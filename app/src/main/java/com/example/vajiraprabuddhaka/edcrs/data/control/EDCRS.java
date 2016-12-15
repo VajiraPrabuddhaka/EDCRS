018ec89cdb9bd803e0efed1582535296f9231de8
Vajira Prabuddhaka <vajiraprabuddhaka@gmail.com>
2016-12-16 00:15:21 +0530

    Database and all the tables are created using SQLite

---
+++ app/src/main/java/com/example/vajiraprabuddhaka/edcrs/data/control/EDCRS.java
@@ -0,0 +1,11 @@
+package com.example.vajiraprabuddhaka.edcrs.data.control;
+
+import java.io.Serializable;
+
+/**
+ * Created by Vajira Prabuddhaka on 12/15/2016.
+ */
+
+public abstract class EDCRS implements Serializable {
+    public abstract void setup();
+}
---
+++ app/src/main/java/com/example/vajiraprabuddhaka/edcrs/data/data/Implementation/DataAcessObjectImplementation.java
@@ -0,0 +1,10 @@
+package com.example.vajiraprabuddhaka.edcrs.data.data.Implementation;
+
+import com.example.vajiraprabuddhaka.edcrs.data.data.DataAccessObject;
+
+/**
+ * Created by Vajira Prabuddhaka on 12/15/2016.
+ */
+
+public class DataAcessObjectImplementation implements DataAccessObject {
+}
